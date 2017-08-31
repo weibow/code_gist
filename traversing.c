b1fb65724e4a5c7091f813376f0455e98bc50a86
kk <you_email@example.com>
2017-08-31 11:17:56 +0800

    Tree data structure

---
+++ traversing.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+typedef char DataType;
+typedef struct node {
+	DataType data;
+	struct node *lchild, *rchild;
+} BinTNode;
+typedef BinTNode *BinTree;
+
+void CreateBinTree(BinTree *T)
+{
+	char ch;
+	
+	if ((ch == getchar()) == ' ')
+		*T = NULL;
+	else {
+		*T = (BinTNode *) malloc(sizeof(BinTNode));
+		(*T)->data = ch;
+		CreateBinTree(&(*T)->lchild);
+		CreateBinTree(&(*T)->rchild);
+	}
+}
+
+typedef enum {Link, Thread} PointerTag;
+typedef struct node {
+	DataType data;
+	PointerTag ltag, rtag;
+	struct node * lchild, *rchild;
+} BinThrNode;
+typedef BinThrNode *BinThrTree;
+BinThrNode *pre = NULL;
+
+void InorderThreading(BinTree p)
+{
+	if (p) {
+		InorderThreading(p->lchild);
+		p->ltag = (p->lchild) ? Link : Thread;
+		p->rtag = (p->rchild) ? Link : Thread;
+		if (pre) {
+			if (pre->rtag == Thread) 
+				pre->rchild = p;
+			if (p->ltag == Thread)
+				p->rchild = pre;
+		}
+		pre = p;
+		InorderThreading(p->rchild);
+	}
+}
+
+void InorderSuccessor(BinThrNode *p)
+{
+	BinThrNode *q;
+
+	if (p->rtag == Thread)
+		return p->rchild;
+	else {
+		q = p->rchild;
+		while (q->ltag == Link)
+			q = q->lchild;
+		return q;
+	}
+}
+
+
+void TraverseInorderThrTree(BinThrTree p)
+{
+	if (p) {
+		while (p->ltag == Link)
+			p = p->lchild;
+		do {
+			printf("%c", p->data);
+			p =  InorderSuccessor(p);
+		} while (p);
+	}
+}
+
+//
+
+#define MaxTreeSize 100
+
+typedef struct cnode {
+	int child;
+	struct cnode * next;
+} CNode;
+
+typedef struct {
+	DataType data;
+	CNode *firstchild;
+} PTNode;
+
+typedef struct {
+	PTNode nodes[MaxTreeSize];
+	int n, root;
+} CTree;
+CTree T;
+
+int main(void)
+{
+}
+
